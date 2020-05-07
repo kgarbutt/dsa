f016103b27e18ef57f0b5169601abb81791cbc6e
Ken Garbutt <kgarbutt@gmail.com>
2020-05-06 18:50:57 -0700

    Python loops

---
+++ loops.py
@@ -0,0 +1,21 @@
+# Python loops
+
+x = 0
+
+while x < 11:
+	print(x)
+	x += 1
+
+print('#'*8)
+	
+words = ['cat', 'dog', 'elephant']
+
+for word in words:
+	print(word)	
+
+print('#'*8)
+
+greet = 'hello world'
+print(greet)
+for i in enumerate(greet[0:11]):print(i)
+
