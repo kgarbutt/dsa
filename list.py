a704a4d9525486df959d6a0eb803756f27aff04e
Ken Garbutt <kgarbutt@gmail.com>
2020-05-06 18:50:28 -0700

    Python lists

---
+++ list.py
@@ -0,0 +1,19 @@
+# Python lists
+
+l = [1,2,3]
+
+print(sum(l))
+print(l[1])
+print(l*2)
+print(l[1:2:2])
+print(max(l))
+print(min(l))
+
+a = ['cat', 'dog', 'elephant']
+
+print(max(a))
+print(min(a))
+
+lr = l.reverse()
+print(lr)
+print(l + a)
