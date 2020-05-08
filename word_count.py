6ec82992b1b241e23eec5cd809c289ad29885587
Ken Garbutt <kgarbutt@gmail.com>
2020-05-08 05:33:35 -0700

    Open txt file & count words using a python dictionary

---
+++ word_count.py
@@ -0,0 +1,21 @@
+# open txt file & count words using a dictionary
+
+count = {}
+#count = dict()
+punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+
+#with open('alice.txt', 'r') as lines:
+with open('Pride_and_Prejudice.txt', 'r') as lines:
+	#print(words.readline())
+	for line in lines:
+		words = line.split()
+		for word in words:
+			if word not in punctuations:
+				if word.isalpha():
+					if word not in count:
+						count[word] = 1
+					else:
+						count[word] += 1
+	
+	print(count)
+	
