90ef12c205bda7aa40b9667173ab2a77d987da6e
Ken Garbutt <kgarbutt@gmail.com>
2020-05-09 09:43:24 -0700

    Open txt file & count words using a default dictionary

---
+++ word_count4.py
@@ -0,0 +1,23 @@
+# open txt file & count words using a default dictionary
+
+from collections import defaultdict
+
+count = defaultdict(int)
+
+punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+
+#with open('alice.txt', 'r') as lines:
+with open('Pride_and_Prejudice.txt', 'r') as lines:
+	#print(words.readline())
+	for line in lines:
+		words = line.split()
+		for word in words:
+			if word not in punctuations:
+				if word.isalpha():
+					if word not in count:
+						count[word] = 1
+					else:
+						count[word] += 1
+	
+	print(count)
+	
